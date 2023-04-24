ba43e0645e84d13fdc03cde91d074d73ba486b4e
Josué Kirchner <josueantoniok@gmail.com>
2023-04-24 19:06:58 -0300

    feat/fix: OrderModel findAll method return updated to have productIds

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -1,5 +1,6 @@
 import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { NewOrder, Order } from '../interfaces/order.interface';
+import { camelizeKeys } from '../utils/camelize';
 
 export default class OrderModel {
   connection: Pool;
@@ -8,27 +9,39 @@ export default class OrderModel {
     this.connection = connection;
   }
 
-  async findAll(): Promise<Order[]> {
+  async findAll(): Promise<any[]> {
     const query = 'SELECT * FROM Trybesmith.orders';
     const [orders] = await this.connection.execute<RowDataPacket[]>(query);
+    
+    const ordersWithProductsIds = await Promise.all(orders.map(async (order) => {
+      const { id } = order;
 
-    return orders as Order[];
+      const queryProducts = 'SELECT * FROM Trybesmith.products WHERE order_id = ?';
+      const [products] = await this.connection.execute<RowDataPacket[]>(queryProducts, [id]);
+
+      return {
+        ...order,
+        productsIds: products.map((product) => product.id),
+      }
+    }));
+
+    return ordersWithProductsIds.map((data) => camelizeKeys(data)) as Order[];
   }
 
   async findById(id: number | string): Promise<Order> {
     const query = 'SELECT * FROM Trybesmith.orders WHERE id = ?';
     const [[order]] = await this.connection.execute<RowDataPacket[]>(query, [id]);
 
-    return order as Order;
+    return camelizeKeys(order) as Order;
   }
 
   async create(order: NewOrder): Promise<Order> {
     const { userId } = order;
 
     const query = 'INSERT INTO Trybesmith.orders (user_id) VALUES (?)';
-    const [{insertId}] = await this.connection.execute<ResultSetHeader>(query, [userId]);
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(query, [userId]);
 
-    return { id: insertId, ...order };
+    return camelizeKeys({ id: insertId, ...order }) as Order;
   }
 
   async update(id: number, order: NewOrder): Promise<Order> {
@@ -37,7 +50,7 @@ export default class OrderModel {
     const query = 'UPDATE Trybesmith.orders SET user_id = ? WHERE id = ?';
     await this.connection.execute<ResultSetHeader>(query, [userId, id]);
 
-    return { id, ...order };
+    return camelizeKeys({ id, ...order }) as Order;
   }
 
   async remove(id: number): Promise<void> {
