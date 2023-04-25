5ccfc972fedde7466bad08ff43121e8e1e26cb7d
Josué Kirchner <josueantoniok@gmail.com>
2023-04-24 22:12:40 -0300

    update: validateToken/jwt to use user.id in req.user

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -1,9 +1,8 @@
-import { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../utils/auth/jwt';
 
 type Middleware = (
-  req: Request & { user?: string | JwtPayload },
+  req: Request & { user?: number },
   res: Response,
   next: NextFunction,
 ) => void;
@@ -17,7 +16,7 @@ const validateToken: Middleware = (req, res, next) => {
 
   const decoded = verifyToken(token);
 
-  req.user = decoded;
+  req.user = decoded || 0;
 
   next();
 };
--- src/utils/auth/jwt.ts
+++ src/utils/auth/jwt.ts
@@ -7,15 +7,18 @@ const jwtConfig: jwt.SignOptions = {
   algorithm: 'HS256',
 };
 
-const generateToken = (user: { id:number }) => {
+const generateToken = (user: { id: number }) => {
   const token = jwt.sign({ id: user.id }, secret, jwtConfig);
 
   return token;
 };
 
-const verifyToken = (token: string) => {
+const verifyToken = (token: string): number => {
   const decoded = jwt.verify(token, secret);
-  return decoded;
+
+  if (typeof decoded !== 'string') return decoded.id || 0;
+
+  return 0;
 };
 
 const decodeToken = (token: string) => {
