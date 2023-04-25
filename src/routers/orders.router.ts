4c0d605ce8e542996bc192f49ec9e9cccfec4c8e
Josué Kirchner <josueantoniok@gmail.com>
2023-04-24 21:21:29 -0300

    feat: endpoint POST /orders created

--- src/routers/orders.router.ts
+++ src/routers/orders.router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import validateToken from '../middlewares/validateToken';
 
 const productController = new OrderController();
 const router = Router();
 
 router.get('/', productController.findAll);
+
+router.post('/', validateToken, productController.create);
 
 export default router;
